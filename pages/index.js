771ec0e0adad8072ce01224f02268d3128cd002e
Robson Vargas <robsonsvargas1993@gmail.com>
2021-12-07 20:22:57 -0300

    Create Cards

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Index() {
   return (
@@ -6,8 +7,78 @@ export default function Index() {
       <Head>
         <title>TreinaCook</title>
         <link rel="stylesheet" href="/styles/page.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        ></link>
       </Head>
-      Olá
+      <header className="header">
+        <Link href="/">
+          <a>
+            <img alt="Logo TreinaCook" src="/img/logo.svg" />
+          </a>
+        </Link>
+      </header>
+
+      <main>
+        <figure className="recipe-card">
+          <Link href="/receitas/doces/brigadeiro">
+            <a>
+              <img
+                className="recipe-picture"
+                src="https://cdn.panelinha.com.br/receita/958014000000-Brigadeiro.jpg"
+                alt="Brigadeiro de festa"
+              />
+            </a>
+          </Link>
+          <div className="recipe-category">Doces</div>
+          <figcaption className="recipe-name">Brigadeiro</figcaption>
+        </figure>
+
+        <figure className="recipe-card">
+          <Link href="/receitas/doces/brigadeiro">
+            <a>
+              <img
+                className="recipe-picture"
+                src="https://cdn.panelinha.com.br/receita/958014000000-Brigadeiro.jpg"
+                alt="Brigadeiro de festa"
+              />
+            </a>
+          </Link>
+          <div className="recipe-category">Doces</div>
+          <figcaption className="recipe-name">Brigadeiro</figcaption>
+        </figure>
+
+        <figure className="recipe-card">
+          <Link href="/receitas/doces/brigadeiro">
+            <a>
+              <img
+                className="recipe-picture"
+                src="https://cdn.panelinha.com.br/receita/958014000000-Brigadeiro.jpg"
+                alt="Brigadeiro de festa"
+              />
+            </a>
+          </Link>
+          <div className="recipe-category">Doces</div>
+          <figcaption className="recipe-name">Brigadeiro</figcaption>
+        </figure>
+
+        <figure className="recipe-card">
+          <Link href="/receitas/doces/brigadeiro">
+            <a>
+              <img
+                className="recipe-picture"
+                src="https://cdn.panelinha.com.br/receita/958014000000-Brigadeiro.jpg"
+                alt="Brigadeiro de festa"
+              />
+            </a>
+          </Link>
+          <div className="recipe-category">Doces</div>
+          <figcaption className="recipe-name">Brigadeiro</figcaption>
+        </figure>
+      </main>
+
+      <footer className="footer">&copy; TreinaCook</footer>
     </div>
   );
 }
